3a5a4c9059596fdf7dfca4b73b3166bb268dfc61
Jonny Paes <jonny.paes@gmail.com>
2025-02-20 17:06:56 -0300

    Update data.js

--- views/static/src/js/funcionario/data.js
+++ views/static/src/js/funcionario/data.js
@@ -6,10 +6,17 @@ function getUrlParameters(parameter, staticURL, decode) {
   // Check if the parameter exists and return its value (decoded if necessary)
   if (urlParams.has(parameter)) {
     let value = urlParams.get(parameter);
+    
+    // If the value is 'false' (string), return boolean false
+    if (value === 'false') {
+      return false;
+    }
+    
+    // Return the value (decoded if necessary)
     return decode ? decodeURIComponent(value) : value;
   }
 
-  // Return false if the parameter is not found
+  // Return boolean false if the parameter is not found
   return false;
 }
 
