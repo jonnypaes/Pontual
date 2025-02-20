dcefc9f79907f1f6e9721aa185befcaad578c3f0
Jonny Paes <jonny.paes@gmail.com>
2025-02-20 12:33:44 -0300

    Update global.js

--- views/static/src/js/funcionario/global.js
+++ views/static/src/js/funcionario/global.js
@@ -6,6 +6,7 @@ var descricao;
 const DEFAULT_STATE = true;
 var checkState = DEFAULT_STATE;
 
+var now = new Date();
 var currentHour = now.getHours();
 var currentMinute = now.getMinutes();
 
@@ -13,3 +14,7 @@ var boolEntrada = false;
 var boolIntervalo = false;
 var boolIntervaloRetorno = false;
 var boolSaida = false;
+
+function getCheckState() {
+    return checkbox.checked;
+}
