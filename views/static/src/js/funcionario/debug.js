8f23c2785c1c8f78789c9deeca5550fbb21398c5
Jonny Paes <jonny.paes@gmail.com>
2025-02-20 16:08:00 -0300

    Update debug.js

--- views/static/src/js/funcionario/debug.js
+++ views/static/src/js/funcionario/debug.js
@@ -1,31 +1,34 @@
 // src/js/funcionario/debug.js
 
 let lineAdjust = ' \n';
-let isDebugMode = true; // Set to false to disable all logging
 let showErrorsOnly = true; // Set to true to show only errors
 
 // Override console.log
 const originalLog = console.log;
 console.log = function(...args) {
-    if (!showErrorsOnly && isDebugMode) {
+    if (isDebugMode) { // Only log if isDebugMode is true
         originalLog.apply(console, args);
-        alert('Console: ' + args.join(' '));
+        if (!showErrorsOnly) {
+            alert('Console: ' + args.join(' '));
+        }
     }
 };
 
 // Override console.error
 const originalError = console.error;
 console.error = function(...args) {
-    if (!showErrorsOnly || args.length > 0) { // Always show errors
+    if (isDebugMode) { // Only log if isDebugMode is true
         originalError.apply(console, args);
-        alert('Error: ' + args.join(' '));
+        if (!showErrorsOnly || args.length > 0) { // Always show errors
+            alert('Error: ' + args.join(' '));
+        }
     }
 };
 
 // Override console.debug
 const originalDebug = console.debug;
 console.debug = function(...args) {
-    if (isDebugMode) {
+    if (isDebugMode) { // Only log if isDebugMode is true
         originalDebug.apply(console, args);
         alert('Debug: ' + args.join(' '));
     }
@@ -33,13 +36,15 @@ console.debug = function(...args) {
 
 // Function to handle errors
 function handleErrors(error) {
-    if (error && !(error instanceof ReferenceError) && !(error instanceof SyntaxError)) {
+    if (isDebugMode && error && !(error instanceof ReferenceError) && !(error instanceof SyntaxError)) {
         alert('Caught Exception: ' + error.name + lineAdjust + error.message + lineAdjust + error.stack);
     }
 }
 
 // Override global error handling
 window.onerror = function(message, source, lineno, colno, error) {
-    handleErrors(error);
-    alert('Global Exception: ' + error.name + lineAdjust + error.message + lineAdjust + error.stack);
+    if (isDebugMode) {
+        handleErrors(error);
+        alert('Global Exception: ' + error.name + lineAdjust + error.message + lineAdjust + error.stack);
+    }
 };
