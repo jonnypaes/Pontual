332befc6fbc002a4eb509ad33bc0deba2aa60f87
Jonny Paes <jonny.paes@gmail.com>
2025-02-27 17:05:38 -0300

    Update global.js

--- views/static/src/js/funcionario/global.js
+++ views/static/src/js/funcionario/global.js
@@ -17,11 +17,6 @@ var language = navigator.languages || navigator.language || navigator.userLangua
 var languagesAvailable = ["pt-BR", "en-US", "es-ES", "ru-RU"];
 var languagesMatched = (navigator.languages || [navigator.language]).find(lang => languagesAvailable.includes(lang)) || languagesAvailable[0];
 
-CanonicalLink = document.createElement('link');
-CanonicalLink.rel = 'canonical';
-CanonicalLink.href = defaultPage;
-document.head.appendChild(link);
-
 function createMetaTag(name, content, isProperty = false) {
     const meta = document.createElement('meta');
     if (isProperty) {
@@ -31,12 +26,20 @@ function createMetaTag(name, content, isProperty = false) {
     }
     meta.setAttribute('content', content);
     document.head.appendChild(meta);
-}
+};
+
+function createLinkTag(rel, href) {
+    const link = document.createElement('link');
+    link.setAttribute('rel', rel);
+    link.setAttribute('href', href);
+    document.head.appendChild(link);
+};
 
 createMetaTag('title', 'O Pontual');
 createMetaTag('og:title', 'O Pontual');
-createMetaTag('og:url', CanonicalLink, true);
+createMetaTag('og:url', currentUrl, true);
 createMetaTag('og:image', `${defaultPage}static/public/graph/512x512.jpg`, true);
 createMetaTag('og:type', 'website', true);
 createMetaTag('description', 'O Pontual - Um aplicativo minimalista para apontamento');
 createMetaTag('og:description', 'O Pontual - Um aplicativo minimalista para apontamento');
+createLinkTag('canonical', currentUrl);
